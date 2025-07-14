80e98632686f65bba04d34dd3a9f0e4b6a1fddfc
oussamabitaa10@gmail.com <oussamabitaa10@gmail.com>
2025-07-14 09:04:40 +0000

    add some configs

---
+++ elgg-config.php
@@ -0,0 +1,26 @@
+<?php
+return [
+    'db' => [
+        'host' => 'localhost',
+        'port' => 3306,
+        'database' => 'elgg_db',
+        'username' => 'oussama',
+        'password' => 'oussama1234',
+    ],
+
+    'site' => [
+        'url' => 'http://10.204.0.16/',
+        'name' => 'Marjane News',
+        'email' => 'oussamabitaa10@gmail.com',
+    ],
+
+    'cache' => [
+        'enabled' => true,
+    ],
+
+    'admin' => [
+        'username' => 'oussama',
+        'password' => 'oussama1234',
+        'email' => 'oussamabitaa10@gmail.com',
+    ],
+];
